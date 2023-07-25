7b735dc686204972794903ce265a3b1896a43954
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2023-07-25 15:19:31 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post"
+
+import styles from './App.module.css'
+
+import './global.css';
+import { Sidebar } from "./components/Sidebar";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+          author="Diogo Oliveira"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio adipisci at ab quidem, eos numquam molestias reprehenderit nesciunt placeat omnis magnam, consequatur cumque tenetur eaque quis, quasi exercitationem? Sequi, fugit?"
+          />
+          <Post 
+          author="Diego Fernandes"
+          content="Um novo post muito legal"
+          />
+        </main>
+      </div>
+    </div>
+  )
+}
